38d644df181d56d3147c0d450c69b498c1408717
fanddrre3 <fandriindranata02@gmail.com>
2024-03-10 20:42:12 +0700

    add function to create grid

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const container = document.getElementsByClassName('container');
 const resetBtn = document.getElementsByClassName('resetBtn');
 
+function createGrid(size) {
+    container.innerHTML = '';
+    container.style.setProperty('--grid-size', size);
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    }
+}
