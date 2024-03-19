d47a38d0dd124b0f9be59e8d51d8f457872de2a2
fanddrre3 <fandriindranata02@gmail.com>
2024-03-19 21:48:53 +0700

    changing style and script file

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-const container = document.getElementsById('container');
-const resetBtn = document.getElementsById('resetBtn');
-
-function createGrid(size) {
-    container.innerHTML = '';
-    container.style.setProperty('--grid-size', size);
-    for (let i = 0; i < size * size; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square);
-    }
-}
-
-function handleHover(e) {
-    e.target.classList.add('hovered');
-}
-
-function resetGrid() {
-    let newSize = parseInt(prompt('Enter the number of squares perside (max 100)'));
-    newSize = Math.min(Math.max(1, newSize), 100);
-    createGrid(newSize);
-}
-
-container.addEventListener('mouseover', handleHover);
-resetBtn.addEventListener('click', resetGrid);
-
-createGrid(16);
+let board = document.querySelector('.board')
+board.computedStyleMap.gridTemplateColumns = 'repeat(16, 1fr)';
+board.computedStyleMap.gridTemplateRows = 'repeat(16, 1fr)';
