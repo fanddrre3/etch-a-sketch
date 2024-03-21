81da00a642b359b5f06bbdd48646390b5835d3ef
fanddrre3 <fandriindranata02@gmail.com>
2024-03-21 22:22:15 +0700

    adding for loop

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let board = document.querySelector('.board')
 board.computedStyleMap.gridTemplateColumns = 'repeat(16, 1fr)';
 board.computedStyleMap.gridTemplateRows = 'repeat(16, 1fr)';
+
+for(let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor
+}
