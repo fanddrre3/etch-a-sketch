1e538481d191c23e76e393d25ceec6bc8ec6aca6
fanddrre3 <fandriindranata02@gmail.com>
2024-03-21 22:24:54 +0700

    adding style

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ board.computedStyleMap.gridTemplateRows = 'repeat(16, 1fr)';
 for(let i = 0; i < 256; i++) {
     let square = document.createElement("div");
     square.style.backgroundColor = "blue";
-    
+    board.insertAdjacentElement('beforeend', square);
 }
