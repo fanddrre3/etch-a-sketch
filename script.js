ef416bedff63eed0fc2ab69a2c6aa33c7b5794af
fanddrre3 <fandriindranata02@gmail.com>
2024-03-21 22:22:57 +0700

    adding script

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ board.computedStyleMap.gridTemplateRows = 'repeat(16, 1fr)';
 
 for(let i = 0; i < 256; i++) {
     let square = document.createElement("div");
-    square.style.backgroundColor
+    square.style.backgroundColor = "blue";
+    
 }
