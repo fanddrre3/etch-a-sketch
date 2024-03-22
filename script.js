b26d78a2080406e0f078b20dfeae9427885150b7
fanddrre3 <fandriindranata02@gmail.com>
2024-03-22 15:29:31 +0700

    adding some js code

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function createGrid() {
     const container = document.querySelector('.container');
     container.innerHTML = '';
     const gridSize = prompt('Enter number of squares per side (max 100):');
+    const squareSize = 100 /gridSize;
 }
