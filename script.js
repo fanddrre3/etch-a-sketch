7564874ca58a73b394b97ff50015beb3f66250c1
fanddrre3 <fandriindranata02@gmail.com>
2024-03-22 15:27:50 +0700

    adding some js code

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-function populateBoard(size) {
-let board = document.querySelector(".board")
-board.computedStyleMap.gridTemplateColumns = `repeat(${size}, 1fr)`;
-board.computedStyleMap.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-for(let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
-    board.insertAdjacentElement("beforeend", square);
-}
-}
-
-populateBoard(16);
-
-function changeSize(input) {
-    populateBoard(input);
+function createGrid() {
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+    const gridSize = prompt('Enter number of squares per side (max 100):');
 }
