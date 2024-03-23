f249c913477a82df4bd345e61847e2edc6066145
fanddrre3 <fandriindranata02@gmail.com>
2024-03-23 19:54:51 +0700

    adding code

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ function createGrid() {
     const gridSize = prompt('Enter number of squares per side (max 100):');
     const squareSize = 100 /gridSize;
 
-    for (let i = 0; i < gridSize * gridSize)
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement('.div');
+        square.classList.add('square');
+        square.style.width = squareSize + '%';
+        square.style.backgroundColor = '#fff';
+        square.addEventListener('mouseover', changeColor);
+        container.appendChild(square);
+    }
 }
