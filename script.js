bc78b7f360d826ba595e9b45aa0ec2f84ad9bc14
fanddrre3 <fandriindranata02@gmail.com>
2024-03-23 19:44:07 +0700

    adding for loop

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ function createGrid() {
     container.innerHTML = '';
     const gridSize = prompt('Enter number of squares per side (max 100):');
     const squareSize = 100 /gridSize;
+
+    for (let i = 0; i < gridSize * gridSize)
 }
